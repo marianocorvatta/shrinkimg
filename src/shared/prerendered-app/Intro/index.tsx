d982a71d3e7ab19661639fe5c44ac9c4085db2f4
Mariano Corvatta <62441854+marianocorvatta@users.noreply.github.com>
2026-02-27 09:21:58 -0300

    Update index.tsx

--- src/shared/prerendered-app/Intro/index.tsx
+++ src/shared/prerendered-app/Intro/index.tsx
@@ -14,7 +14,7 @@ import smallSectionAsset from 'url:./imgs/info-content/small.svg';
 import simpleSectionAsset from 'url:./imgs/info-content/simple.svg';
 import secureSectionAsset from 'url:./imgs/info-content/secure.svg';
 import logoIcon from 'url:./imgs/demos/icon-demo-logo.png';
-import logoWithText from 'url:./imgs/logo-with-text.png';
+import logoImg from 'url:static-build/assets/logo-w.png';
 import * as style from './style.css';
 import type SnackBarElement from 'shared/custom-els/snack-bar';
 import 'shared/custom-els/snack-bar';
@@ -161,7 +161,7 @@ export default class Intro extends Component<Props, State> {
           <h1 class={style.logoContainer}>
             <img
               class={style.logo}
-              src={logoWithText}
+              src={logoImg}
               alt="ShrinkImg — Free Online Image Compressor"
             />
             <span class={style.logoBrandName}>ShrinkImg</span>
